3fcc3d86c90c4705be471f035fd481785ffd2493
EGabriel203 <104772266+EGabriel203@users.noreply.github.com>
2022-10-12 18:44:34 -0300

    Add files via upload

---
+++ TestaPontoFlutuantes.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuantes {
+	
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu salario eh " + salario);
+		
+		
+		double divisao = 3.14 / 2;
+		System.out.println("o resultado eh " + divisao);	
+		
+		int outraDivisao = 5 / 2;
+		System.out.println("resultado eh " + outraDivisao);
+		
+		double novaTentativa = 5 / 2;
+		System.out.println(novaTentativa);
+		
+
+    }
+}
